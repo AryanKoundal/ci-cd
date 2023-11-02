b99324835a7deda20d21f07c51d9f5f65f0826d3
aryan <aryankoundal474@gmail.com>
2023-11-02 22:01:09 +0530

    removed sudo from deploy.sh

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "ci-cd-key.pem" ubuntu@ec2-51-20-130-15.eu-north-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
+ssh -t -i "ci-cd-key.pem" ubuntu@ec2-51-20-130-15.eu-north-1.compute.amazonaws.com  "bash ~/deploy.sh"
