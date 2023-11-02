c1741368d9eb436cceaf9d5cd4e567ff3ad945f9
aryan <aryankoundal474@gmail.com>
2023-11-02 15:55:18 +0530

    change mongo code

--- server/index.js
+++ server/index.js
@@ -19,6 +19,6 @@ app.get("", (req, res) => {
 
 // uniform resource identifier
 var uri = "mongodb+srv://aryankoundal:lHKDAlmlT7KvRQP0@clustername-cluster0.cupcng4.mongodb.net/";
-mongoose.connect(uri, { useUnifiedTopology: true, dbName: "ci-cd" });
+mongoose.connect(uri, { useUnifiedTopology: true, dbName: "ci-cd-aws" });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
