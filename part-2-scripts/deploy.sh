c8fc7c7342898e4388886ec8aa95ffd5dc773d17
aryan <aryankoundal474@gmail.com>
2023-12-01 22:16:49 +0530

    rerunning the server

--- part-2-scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.1.0/bin
-
-cd ci-cd
- git pull origin main
- cd server
- pm2 kill
- pm2 start index.js
--- part-2-scripts/script-local.sh
+++
@@ -1,1 +0,0 @@
-ssh -t -i "ci-cd-key.pem" ubuntu@ec2-51-20-130-15.eu-north-1.compute.amazonaws.com  "bash ~/deploy.sh"
